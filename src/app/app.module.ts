b8e78a0dd52c9dd40e6dbec0fe941fe5dcb50c3d
Merijn <merijn@mijnmoes.nl>
2018-09-20 14:38:47 +0200

    Added twitter component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { FeatureComponent } from "./featured/feature.component";
 import { DetailComponent } from "./detail/detail.component";
+import { TwitterComponent } from "./featured/twitter/twitter.component";
 
 @NgModule({
-  declarations: [AppComponent, FeatureComponent, DetailComponent],
+  declarations: [AppComponent, FeatureComponent, DetailComponent, TwitterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
