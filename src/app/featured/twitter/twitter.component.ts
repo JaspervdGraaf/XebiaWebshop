ebe6c1f0d193e3e2f76579bd6aca6f67e92f8a27
Merijn <merijn@mijnmoes.nl>
2018-09-20 14:57:52 +0200

    Added twitter script

--- src/app/featured/twitter/twitter.component.ts
+++ src/app/featured/twitter/twitter.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
 @Component({
     selector: "app-twitter",
     templateUrl: "./twitter.component.html",
     styleUrls: ["./twitter.component.scss"]
   })
-  export class TwitterComponent implements OnInit {
+  export class TwitterComponent implements OnInit, AfterViewInit {
     constructor() {}
   
     ngOnInit(): void {}
+    
+    ngAfterViewInit () {
+      !function(d,s,id){
+          var js: any,
+              fjs=d.getElementsByTagName(s)[0],
+              p='https';
+          if(!d.getElementById(id)){
+              js=d.createElement(s);
+              js.id=id;
+              js.src=p+"://platform.twitter.com/widgets.js";
+              fjs.parentNode.insertBefore(js,fjs);
+          }
+      }
+      (document,"script","twitter-wjs");
+}
   }
   
