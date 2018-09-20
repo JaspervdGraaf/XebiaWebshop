c6c45c2aa0ca84147712e8c11456b272cc44a9e1
Jasper van der Graaf <jaspervdgraaf@gmail.com>
2018-09-20 14:38:09 +0200

    add components superwoman and superman

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { FeatureComponent } from "./featured/feature.component";
 import { DetailComponent } from "./detail/detail.component";
+import { SupermanComponent } from './superman/superman.component';
+import { SuperwomanComponent } from './superwoman/superwoman.component';
 
 @NgModule({
-  declarations: [AppComponent, FeatureComponent, DetailComponent],
+  declarations: [AppComponent, FeatureComponent, DetailComponent, SupermanComponent, SuperwomanComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FeatureComponent } from "./featured/feature.component";
 import { DetailComponent } from "./detail/detail.component";
+import { SuperwomanComponent } from "./superwoman/superwoman.component";
+import { SupermanComponent } from "./superman/superman.component";
 
 const routes: Routes = [
   {
@@ -17,6 +19,14 @@ const routes: Routes = [
   {
     path: "detail",
     component: DetailComponent
+  },
+  {
+    path: "superman",
+    component: SupermanComponent
+  },
+  {
+    path: "superwoman",
+    component: SuperwomanComponent
   }
 ];
 
